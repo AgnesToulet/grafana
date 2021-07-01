cc7697cb2b48dab56f3bb724226e702ac46ccbfa
Agnès Toulet <agnes.toulet@gmail.com>
2021-07-01 16:00:42 +0200

    Get all plugins in UpdatePluginSetting

--- pkg/api/plugins.go
+++ pkg/api/plugins.go
@@ -150,11 +150,8 @@ func (hs *HTTPServer) GetPluginSettingByID(c *models.ReqContext) response.Respon
 func (hs *HTTPServer) UpdatePluginSetting(c *models.ReqContext, cmd models.UpdatePluginSettingCmd) response.Response {
 	pluginID := c.Params(":pluginId")
 
-	if app := hs.PluginManager.GetApp(pluginID); app == nil {
-		vcs := hs.PluginManager.VersionedControlStorage()
-		if vcs == nil || vcs.Id != pluginID {
-			return response.Error(404, "Plugin not installed", nil)
-		}
+	if plugin := hs.PluginManager.GetPlugin(pluginID); plugin == nil {
+		return response.Error(404, "Plugin not installed", nil)
 	}
 
 	cmd.OrgId = c.OrgId
