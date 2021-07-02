bc1fed8e9e162bca713e754dce49a3cd60f7195a
gamab <gabi.mabs@gmail.com>
2021-07-02 16:03:16 +0200

    vcs.go file

---
+++ pkg/api/vcs.go
@@ -0,0 +1,29 @@
+package api
+
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/grafana/grafana/pkg/services/vcs"
+)
+
+func (hs *HTTPServer) storeObjInVCS(ctx context.Context, kind vcs.Kind, uid string, obj interface{}) error {
+	if hs.VCS.IsDisabled() {
+		return nil
+	}
+
+	dashJson, err := json.Marshal(obj)
+	if err != nil {
+		return err
+	}
+
+	vobj := vcs.VersionedObject{
+		ID:   uid,
+		Kind: vcs.Dashboard,
+		Data: dashJson,
+	}
+
+	_, err = hs.VCS.Store(ctx, vobj)
+
+	return err
+}
